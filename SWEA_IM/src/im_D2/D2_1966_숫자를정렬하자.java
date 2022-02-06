9dd641ca56899aca528598f3f3d2fcc736a634ea
jg6735 <jg6735@naver.com>
2022-02-06 16:49:41 +0900

    SWEA IM_D2 1966 정렬

--- SWEA_IM/src/im_D2/D2_1966_숫자를정렬하자.java
+++ SWEA_IM/src/im_D2/D2_1966_숫자를정렬하자.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class D2_1966_숫자를정렬하자 {
@@ -24,15 +25,29 @@ public class D2_1966_숫자를정렬하자 {
 				arr[i] = Integer.parseInt(st.nextToken());
 			}
 			
-			for (int i = 0; i < N; i++) {
-				for (int j = 0; j < N; j++) {
-					if (arr[j] > arr[i]) {
-						temp = arr[i];
-						arr[i] = arr[j];
-						arr[j] = temp;
-					}
-				}
-			}
+//			for (int i = 0; i < N; i++) {
+//				for (int j = 0; j < N; j++) {
+//					if (arr[j] > arr[i]) {
+//						temp = arr[i];
+//						arr[i] = arr[j];
+//						arr[j] = temp;
+//					}
+//				}
+//			}
+			
+//			// 버블 정렬
+//			for (int i = 0; i < N; i++) {
+//				for (int j = 0; j < N - 1 - i; j++) {
+//					if (arr[j] > arr[j + 1]) {
+//						temp = arr[j];
+//						arr[j] = arr[j + 1];
+//						arr[j + 1] = temp;
+//					}
+//				}
+//			}
+			
+			// Arrays API
+			Arrays.sort(arr);
 			
 			System.out.printf("#%d ", tc);
 			for (int i = 0; i < N; i++) {
